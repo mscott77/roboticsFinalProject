24aa3deef8ad91f2319ff8f8e4b7ce33f74f22e8
Mason Scott <masonscott2112@gmail.com>
2024-11-26 08:50:46 -0700

    wrote some more tests

--- test_navScene.py
+++ test_navScene.py
@@ -18,7 +18,7 @@ def test_1_checkIfConfigIsInCollision():
 
     target = (0,6)
     arm = kin.SerialArm(dh,joint_types)
-    navScene = NavigationScene(arm,obstacle,[0,0],target,linkWidth)
+    navScene = NavigationScene(arm,obstacle,jointConfig,target,linkWidth)
 
     isCollision = navScene._checkIfConfigIsInCollision(jointConfig)
     assert isCollision == True
@@ -39,7 +39,7 @@ def test_2_checkIfConfigIsInCollision():
 
     target = (0,6)
     arm = kin.SerialArm(dh,joint_types)
-    navScene = NavigationScene(arm,obstacle,[0,0],target,linkWidth)
+    navScene = NavigationScene(arm,obstacle,jointConfig,target,linkWidth)
 
     isCollision = navScene._checkIfConfigIsInCollision(jointConfig)
     assert isCollision == True
@@ -60,7 +60,7 @@ def test_3_checkIfConfigIsInCollision():
 
     target = (0,6)
     arm = kin.SerialArm(dh,joint_types)
-    navScene = NavigationScene(arm,obstacle,[0,0],target,linkWidth)
+    navScene = NavigationScene(arm,obstacle,jointConfig,target,linkWidth)
 
     isCollision = navScene._checkIfConfigIsInCollision(jointConfig)
     assert isCollision == True
@@ -81,7 +81,7 @@ def test_4_checkIfConfigIsInCollision():
 
     target = (0,6)
     arm = kin.SerialArm(dh,joint_types)
-    navScene = NavigationScene(arm,obstacle,[0,0],target,linkWidth)
+    navScene = NavigationScene(arm,obstacle,jointConfig,target,linkWidth)
 
     isCollision = navScene._checkIfConfigIsInCollision(jointConfig)
     assert isCollision == True
@@ -102,7 +102,7 @@ def test_5_checkIfConfigIsInCollision():
 
     target = (0,6)
     arm = kin.SerialArm(dh,joint_types)
-    navScene = NavigationScene(arm,obstacle,[0,0],target,linkWidth)
+    navScene = NavigationScene(arm,obstacle,jointConfig,target,linkWidth)
 
     isCollision = navScene._checkIfConfigIsInCollision(jointConfig)
     assert isCollision == True
@@ -111,6 +111,7 @@ def test_5_checkIfConfigIsInCollision():
     if __name__ == "__main__":
         navScene.drawScene()
 
+# kissing obstacle test
 def test_6_checkIfConfigIsInCollision():
     jointConfig = [0,0]
     obstacle = Obstacle([14.25,0,0],2)
@@ -123,7 +124,7 @@ def test_6_checkIfConfigIsInCollision():
 
     target = (0,6)
     arm = kin.SerialArm(dh,joint_types)
-    navScene = NavigationScene(arm,obstacle,[0,0],target,linkWidth)
+    navScene = NavigationScene(arm,obstacle,jointConfig,target,linkWidth)
 
     isCollision = navScene._checkIfConfigIsInCollision(jointConfig)
     assert isCollision == True
@@ -133,7 +134,7 @@ def test_6_checkIfConfigIsInCollision():
         navScene.drawScene()
 
 # bigger spheres for representing the arm (bigger link width) so that the circles show up in visulization
-def test_7_checkIfConfigIsInCollision():
+def test_601_checkIfConfigIsInCollision():
     jointConfig = [0,0]
     obstacle = Obstacle([14.5,0,0],2)
     linkWidth = 1
@@ -145,7 +146,7 @@ def test_7_checkIfConfigIsInCollision():
 
     target = (0,6)
     arm = kin.SerialArm(dh,joint_types)
-    navScene = NavigationScene(arm,obstacle,[0,0],target,linkWidth)
+    navScene = NavigationScene(arm,obstacle,jointConfig,target,linkWidth)
 
     isCollision = navScene._checkIfConfigIsInCollision(jointConfig)
     assert isCollision == True
@@ -156,14 +157,106 @@ def test_7_checkIfConfigIsInCollision():
         navScene.drawScene(drawCollisionCircles=True)
         navScene.drawScene(drawArm=False,drawCollisionCircles=True)
 
-# kissing obstacle test
-
 # kissing obstacle in different joint configuration
+def test_7_checkIfConfigIsInCollision():
+    jointConfig = [0,np.pi/2]
+    obstacle = Obstacle([6,8.25,0],2)
+    linkWidth = 0.5
+
+    link_length = 6 
+    dh = [  [0,0,link_length,0],
+            [0,0,link_length,0]]
+    joint_types = ['r','r']
+
+    target = (0,6)
+    arm = kin.SerialArm(dh,joint_types)
+    navScene = NavigationScene(arm,obstacle,jointConfig,target,linkWidth)
+
+    isCollision = navScene._checkIfConfigIsInCollision(jointConfig)
+    assert isCollision == True
 
 
+    if __name__ == "__main__":
+        navScene.drawScene()
+        navScene.drawScene(drawCollisionCircles=True)
+        navScene.drawScene(drawArm=False,drawCollisionCircles=True)
 
+# just shy of kissing
+def test_701_checkIfConfigIsInCollision():
+    jointConfig = [0,np.pi/2]
+    obstacle = Obstacle([6,8.26,0],2)
+    linkWidth = 0.5
+
+    link_length = 6 
+    dh = [  [0,0,link_length,0],
+            [0,0,link_length,0]]
+    joint_types = ['r','r']
+
+    target = (0,6)
+    arm = kin.SerialArm(dh,joint_types)
+    navScene = NavigationScene(arm,obstacle,jointConfig,target,linkWidth)
+
+    isCollision = navScene._checkIfConfigIsInCollision(jointConfig)
+    assert isCollision == False
+
+
+    if __name__ == "__main__":
+        navScene.drawScene()
+        navScene.drawScene(drawCollisionCircles=True)
+        navScene.drawScene(drawArm=False,drawCollisionCircles=True)
+
+# different link lengths, link width, object size. kissing
+def test_8_checkIfConfigIsInCollision():
+    jointConfig = [0,np.pi/2]
+    obstacle = Obstacle([5,2,0],0.5)
+    linkWidth = 1
+    #
+    link1_length = 4 
+    link2_length = 2
+
+    dh = [  [0,0,link1_length,0],
+            [0,0,link2_length,0]]
+    joint_types = ['r','r']
+
+    target = (0,6)
+    arm = kin.SerialArm(dh,joint_types)
+    navScene = NavigationScene(arm,obstacle,jointConfig,target,linkWidth)
+
+    isCollision = navScene._checkIfConfigIsInCollision(jointConfig)
+    assert isCollision == True
+
+
+    if __name__ == "__main__":
+        navScene.drawScene()
+        navScene.drawScene(drawCollisionCircles=True)
+        navScene.drawScene(drawArm=False,drawCollisionCircles=True)
+
+def test_801_checkIfConfigIsInCollision():
+    jointConfig = [0,np.pi/2]
+    obstacle = Obstacle([5.2,2,0],0.5)
+    linkWidth = 1
+    #
+    link1_length = 4 
+    link2_length = 2
+
+    dh = [  [0,0,link1_length,0],
+            [0,0,link2_length,0]]
+    joint_types = ['r','r']
+
+    target = (0,6)
+    arm = kin.SerialArm(dh,joint_types)
+    navScene = NavigationScene(arm,obstacle,jointConfig,target,linkWidth)
+
+    isCollision = navScene._checkIfConfigIsInCollision(jointConfig)
+    assert isCollision == False
+
+
+    if __name__ == "__main__":
+        navScene.drawScene()
+        navScene.drawScene(drawCollisionCircles=True)
+        navScene.drawScene(drawArm=False,drawCollisionCircles=True)
 
 # run tests from here to see visualization (if there is one)
 if __name__ == "__main__":
-    test_7_checkIfConfigIsInCollision()
+    test_801_checkIfConfigIsInCollision()
         
